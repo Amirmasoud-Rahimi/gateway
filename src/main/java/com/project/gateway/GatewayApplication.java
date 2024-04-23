587094652967eb09035583e6d3abcb7c66afad1d
Amirmasoud-Rahimi <amirmasoudrahimiofficial@gmail.com>
2024-04-23 15:06:54 +0330

    Initial commit

---
+++ src/main/java/com/project/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package com.project.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient // to register as a service in eureka server
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
